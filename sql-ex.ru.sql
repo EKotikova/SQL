ebea5e22fa42ace9b1149b72991bdb0511436cca
Kotikova Elena <elenakotikova2021@gmail.com>
2021-12-03 18:31:56 +0300

    Update sql-ex.ru.sql

--- sql-ex.ru.sql
+++ sql-ex.ru.sql
@@ -21,11 +21,7 @@ SELECT model, speed, hd FROM PC WHERE cd IN('12x','24x' ) AND price<600
  /*6  Для каждого производителя, 
 выпускающего ПК-блокноты c объёмом жесткого диска не менее 10 Гбайт, 
 найти скорости таких ПК-блокнотов. Вывод: производитель, скорость. */
-
-
 Select DISTINCT Maker,Speed from Product AS p JOIN Laptop AS l ON P.Model=l.Model where hd>=10
-
-
 
  /*7 Найдите номера моделей и цены всех имеющихся в продаже продуктов (любого типа) производителя B (латинская буква).*/
 
@@ -35,3 +31,30 @@ UNION Select model,price from  printer) AS t JOIN Product AS
 p ON p.model=t.model where maker='B'
 Select model,price from Laptop 
 p ON p.model=t.model where maker='B'
+
+/*8 Найдите производителя, выпускающего ПК, но не ПК-блокноты.*/
+Select distinct maker from Product where type='pc' except select maker from product where type = 'laptop'
+/*9 Найдите производителей ПК с процессором не менее 450 Мгц. Вывести: Maker*/
+Select distinct maker  from pc inner join product on pc.model = product.model where 
+speed>=450
+/*10 Найдите модели принтеров, имеющих самую высокую цену. Вывести: model, price */
+SELECT model, price FROM Printer where price=(Select MAX(price) from Printer)
+/*11 Найдите среднюю скорость ПК.*/
+Select AVG(speed) from PC
+/*12 Найдите среднюю скорость ПК-блокнотов, цена которых превышает 1000 дол.*/
+Select AVG(speed) from Laptop where price>1000
+/* 13 Найдите среднюю скорость ПК, выпущенных производителем A.*/
+Select avg(speed) from pc inner join product on pc.model= product.model where maker = 'A'
+/* 14 Найдите класс, имя и страну для кораблей из таблицы Ships, имеющих не менее 10 орудий.*/
+Select classes.class ,name,country from classes inner join ships on classes.class = ships.class  
+where numguns >= 10
+/*15 Найдите размеры жестких дисков, совпадающих у двух и более PC. Вывести: HD*/
+Select hd from PC group by hd having count(model)>1
+
+
+
+
+
+
+
+
